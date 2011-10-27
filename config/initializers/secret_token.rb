cdd5839504b9e041d3286aa68bd40bcdf8cc2a0e
Justin Cunningham <justin@compucatedsolutions.com>
2011-10-27 14:47:54 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseMailchimpDemo::Application.config.secret_token = '175388c0574461e0fa67b54e56f5a8fd7b94465576af0228887129fb4a455cfe23c2310cb33ce8b18ae4b81b6670dcc5f626971b02ffc3725f4151408288e3a9'
